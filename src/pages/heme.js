ef1cddd8cdd161a84d4ca17b941fefd6680dbd0c
luis91cc <luis@lopezvicu.es>
2025-02-23 15:49:33 +0100

    Create heme.js

---
+++ src/pages/heme.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+
+function Home() {
+  return (
+    <div className="page-container home-container">
+      <h1>Bienvenido a #SoyReal</h1>
+      <p>Un espacio para promover la autoestima y la autoaceptación.</p>
+      <a href="https://drive.google.com/drive/folders/1l12zJYch3312E1tzOQEFi2dwV3cWM66K?usp=sharing" target="_blank" rel="noopener noreferrer">
+        <button>Sonrisa</button>
+      </a>
+    </div>
+  );
+}
+
+export default Home;
