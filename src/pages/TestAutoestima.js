2836d758841e1930a300c415d1ad629da9fe066d
luis91cc <luis@lopezvicu.es>
2025-02-23 15:52:18 +0100

    Create TestAutoestima.js

---
+++ src/pages/TestAutoestima.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+
+function TestAutoestima() {
+  return (
+    <div className="page-container test-container">
+      <h1>Test de Autoestima</h1>
+      <p>Evalúa tu autoestima con nuestro test.</p>
+      <a href="https://drive.google.com/drive/folders/1uGbtRLvdBCVybKLDAgWgL621Oc3ltdu2?usp=sharing" target="_blank" rel="noopener noreferrer">
+        <button>Iniciar Test</button>
+      </a>
+    </div>
+  );
+}
+
+export default TestAutoestima;
