2e0a162e9540adb2dda8c75face8c50ed0082357
luis91cc <luis@lopezvicu.es>
2025-02-23 15:50:34 +0100

    Create recursos.js

---
+++ src/pages/recursos.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Recursos() {
+  return (
+    <div className="page-container recursos-container">
+      <h1>Recursos sobre Autoestima</h1>
+      <p>Encuentra artículos, libros y videos que te ayudarán a fortalecer tu autoestima.</p>
+      <a 
+        href="https://drive.google.com/drive/folders/1mxcWpNSYUikFynWTRx2wt_cugMgLoMTO?usp=sharing" 
+        target="_blank" 
+        rel="noopener noreferrer"
+      >
+        <button>Biblioteca</button>
+      </a>
+    </div>
+  );
+}
+
+export default Recursos;
