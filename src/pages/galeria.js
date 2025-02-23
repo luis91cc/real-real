0d8c421205202ffdb53aa1fefc51ff3eb68d8a75
luis91cc <luis@lopezvicu.es>
2025-02-23 15:49:08 +0100

    Create galeria.js

---
+++ src/pages/galeria.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+
+function Galeria() {
+  return (
+    <div className="page-container galeria-container">
+      <h1>Galería</h1>
+      <p>Explora imágenes y relatos inspiradores.</p>
+      <a href="https://drive.google.com/drive/folders/1GrngXu_UpeEKFjMA9itSzT0g0p0eEkF-?usp=sharing" target="_blank" rel="noopener noreferrer">
+        <button>Ver Galería</button>
+      </a>
+    </div>
+  );
+}
+
+export default Galeria;
