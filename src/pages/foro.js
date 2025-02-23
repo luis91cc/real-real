9aae02ae28afa24129c9620b54238c741c3009c9
luis91cc <luis@lopezvicu.es>
2025-02-23 15:48:28 +0100

    Create foro.js

---
+++ src/pages/foro.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+
+function Foro() {
+  return (
+    <div className="page-container foro-container">
+      <h1>Foro</h1>
+      <p>Comparte experiencias y apoya a otros.</p>
+      <a href="https://drive.google.com/drive/folders/1c_rpBXpNfsLmH7a3GKcrRKTmaO7C5yKw?usp=sharing" target="_blank" rel="noopener noreferrer">
+        <button>Participar</button>
+      </a>
+    </div>
+  );
+}
+
+export default Foro;
